b121c495f5e1dd89f98377a8b5ba6e9d4bb051fa
MalachiCox <coxmalachi04@gmail.com>
2024-03-04 13:52:02 -0600

    JS Homework

--- JS1-1.js
+++ JS1-1.js
@@ -13,11 +13,32 @@
 //* 1. Cube Number Test
 // Print out all cubed numbers up to the total value 1000. Meaning that if the cubed number is over 1000 break the loop.
 
+let n = 0;
+
+while (n * n * n <= 1000) {
+    console.log(n*n*n);
+    n++;
+}
+console.log('Done')
 
 //* 2. Prime Numbers
 // Get first prime numbers up to 100
 
+for (let i = 1; i <= 100; i+=2) {
+    console.log(i)
+}
+console.log('Done')
 
 //* 3. Age Question
 // Take in a users input for their age, if they are younger than 18 log kids, 
 //if they're 18 to 65 log adults, else log seniors
+
+let age = 19;
+if (age < 18) {
+    console.log('kids')
+} else if (age < 65) {
+    console.log('adults')
+} else {
+    console.log('seniors')
+}
+console.log('Done')
